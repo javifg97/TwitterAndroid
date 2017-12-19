45790f92601d498a3a854556defa0e2a153892ea
Guillermo Rodriguez Sainz <guillermo.sainz@live.u-tad.com>
2017-12-19 12:46:29 +0100

    ex3: FBNoticia completado

---
+++ examen1/src/main/java/com/example/guille/examen1/FBObjects/FBNoticia.java
@@ -0,0 +1,20 @@
+package com.example.guille.examen1.FBObjects;
+
+/**
+ * Created by guille on 19/12/17.
+ */
+
+public class FBNoticia {
+
+    public String Titulo;
+    public String Noticia;
+
+    public FBNoticia(){
+
+    }
+
+    public FBNoticia(String titulo,String noticia){
+        this.Titulo=titulo;
+        this.Noticia=noticia;
+    }
+}
